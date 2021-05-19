d7e09ae9d9b682c4c3277f92598b982080b1bea3
developiaa <developiaa@gmail.com>
2021-05-19 14:35:59 +0900

    Add polymorphism study

---
+++ src/polymorphism.java
@@ -0,0 +1,40 @@
+public class polymorphism {
+    public static void main(String[] args) {
+        Parent p = new Child();
+        Child c = new Child();
+
+        // (자식클래스에서 메소드가 오버라이딩 되어 있다는 가정하에)
+        // 멤버 변수, static 메소드는 현재 참조 변수의 타입(Parent)을 따른다.
+        System.out.println("p.x = " + p.x);
+        System.out.println("p.k = " + p.k);
+
+        // 조상타입(Parent)의 참조 변수가 인스턴스 메서드를 호출시에는 실제 인스턴스(Child)를 따른다.
+        p.method();
+        System.out.println();
+
+
+        System.out.println("c.x = " + c.x);
+        System.out.println("p.k = " + c.k);
+        c.method();
+    }
+}
+
+
+class Parent {
+    static int k = 400;
+    int x = 100;
+
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child extends Parent {
+    static int k = 500;
+    int x = 200;
+
+    void method() {
+        System.out.println("Child Method");
+    }
+}
+
---
+++ src/polymorphism2.java
@@ -0,0 +1,46 @@
+public class polymorphism2 {
+    public static void main(String[] args) {
+        Parent2 p = new Child2();
+        Child2 c = new Child2();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+        System.out.println();
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+
+}
+
+class Parent2 {
+    int x = 100;
+
+    void method() {
+        System.out.println("Parent Method");
+    }
+}
+
+class Child2 extends Parent2 {
+    int x = 200;
+
+    void method() {
+        System.out.println("x=" + x);	// this.x와 같다.
+        System.out.println("super.x=" + super.x);
+        System.out.println("this.x=" + this.x);
+    }
+}
+
+
+
+
+
+
+//p.x = 100
+//x=200
+//super.x=100
+//this.x=200
+//
+//c.x = 200
+//x=200
+//super.x=100
+//this.x=200
