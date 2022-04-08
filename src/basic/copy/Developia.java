1c07350203229d5d781585fe96336cb872b36a9e
developiaa <developiaa@gmail.com>
2022-04-09 00:23:56 +0900

    feat : Add shallow/deep copy test

---
+++ src/basic/copy/Developia.java
@@ -0,0 +1,63 @@
+package basic.copy;
+
+public class Developia implements Cloneable{
+    public String name;
+    public Integer money;
+
+    public Developia() {
+    }
+
+    public Developia(String name, Integer money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // 실무에서는 해당 메소드가 제한 될 수 있음
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    // 금액 차감
+    public void spendMoney(Integer money) {
+        this.money -= money;
+    }
+
+    @Override
+    public String toString() {
+        return "Developia{" +
+                "money=" + money +
+                '}';
+    }
+
+    // 복사 생성자를 이용한 방법
+    public Developia(Developia developia) {
+        this.name = developia.name;
+        this.money = developia.money;
+    }
+
+    // 복사 팩토리 메소드를 이용한 방법
+    public static Developia newObject(Developia developia) {
+        // 기본 생성자 필요
+        Developia d = new Developia();
+        d.name = developia.name;
+        d.money = developia.money;
+        return d;
+    }
+
+    @Override
+    public Developia clone() throws CloneNotSupportedException {
+        return (Developia) super.clone();
+    }
+}
---
+++ src/basic/copy/test/CopyTest.java
@@ -0,0 +1,82 @@
+package basic.copy.test;
+
+import basic.copy.Developia;
+
+public class CopyTest {
+    public static void main(String[] args) {
+        Developia developiaMain = new Developia("A", 10000);
+
+        /*
+         * 얕은 복사(shallow copy)
+         * 주소값을 복사하는 얕은 복사가 이루어짐
+         */
+
+        Developia developia1 = developiaMain;
+
+        developia1.spendMoney(1000);
+        System.out.println("developia1 = " + developia1);
+        // 2의 돈을 차감했지만 1도 변화된 것을 알 수 있다.
+        System.out.println("developiaMain = " + developiaMain);
+        System.out.println();
+
+        /*
+         * 깊은 복사(deep copy)
+         * 1. 복사 생성자를 이용한 방법
+         */
+        Developia developiaMain2 = new Developia("A", 10000);
+        Developia developia2 = new Developia();
+        developia2.setName(developiaMain2.getName());
+        // 실무에서는 setMoney와 같은 동작이 제한 될 수 있음
+        developia2.setMoney(developiaMain2.getMoney());
+
+        developia2.spendMoney(1000);
+        System.out.println("developia2 = " + developia2);
+        System.out.println("developiaMain2 = " + developiaMain2);
+        System.out.println();
+
+        /*
+         * 깊은 복사(deep copy)
+         * 2. 복사 팩토리 메소드를 이용한 방법
+         */
+        Developia developiaMain3 = new Developia("A", 10000);
+        Developia developia3 = Developia.newObject(developiaMain3);
+
+        developia3.spendMoney(1000);
+        System.out.println("developia3 = " + developia3);
+        System.out.println("developiaMain3 = " + developiaMain3);
+        System.out.println();
+
+        /*
+         * 깊은 복사(deep copy)
+         * 3. 직접 객체 생성
+         */
+        Developia developiaMain4 = new Developia("A", 10000);
+        Developia developia4 = new Developia();
+        developia4.setName(developiaMain4.getName());
+        // 마찬가지로 set 메소드가 제한될 수 있음
+        developia4.setMoney(developiaMain4.getMoney());
+
+        developia4.spendMoney(1000);
+        System.out.println("developia4 = " + developia4);
+        System.out.println("developiaMain4 = " + developiaMain4);
+        System.out.println();
+
+        /*
+         * 깊은 복사(deep copy)
+         * 4. Cloneable interface 구현
+         */
+        Developia developiaMain5 = new Developia("A", 10000);
+        // final 인스턴스나 배열이 아닌 경우에는 추천되지 않음
+        try {
+            Developia developia5 = developiaMain5.clone();
+            developia5.spendMoney(1000);
+
+            System.out.println("developia5 = " + developia5);
+            System.out.println("developiaMain5 = " + developiaMain5);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
