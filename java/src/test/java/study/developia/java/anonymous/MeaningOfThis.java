b7d459437836aeafe86631cc1eaca0dad1438534
developiaa <developiaa@gmail.com>
2023-12-06 23:42:47 +0900

    feat: 익명 객체

---
+++ java/src/test/java/study/developia/java/anonymous/MeaningOfThis.java
@@ -0,0 +1,23 @@
+package study.developia.java.anonymous;
+
+public class MeaningOfThis {
+    public final int value = 4;
+    public void doIt() {
+        int value = 6;
+        Runnable r = new Runnable() {
+            public final int value = 5;
+
+            @Override
+            public void run() {
+                int value = 10;
+                System.out.println(this.value);
+            }
+        };
+        r.run();
+
+    }
+    public static void main(String[] args) {
+        MeaningOfThis meaningOfThis = new MeaningOfThis();
+        meaningOfThis.doIt(); //5
+    }
+}
