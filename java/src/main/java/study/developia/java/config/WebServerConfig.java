08f1e321ba8de05b819989c6a17c845303939104
developiaa <developiaa@gmail.com>
2024-05-12 10:00:00 +0900

    feat: locale 인터셉터

---
+++ java/src/main/java/study/developia/java/config/WebServerConfig.java
@@ -0,0 +1,32 @@
+package study.developia.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class WebServerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREAN); // 헤더에 Accept-language
+        return localeResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("locale"); // paramerter로 locale 설정
+        registry.addInterceptor(localeChangeInterceptor)
+                .excludePathPatterns("/favicon.ico")
+                .addPathPatterns("/**");
+
+    }
+}
