e37c87732b87489649f27845b637ebfd674a27cf
developiaa <developiaa@gmail.com>
2024-09-02 20:00:00 +0900

    feat: [advance1] 스레드 시작1

---
+++ advance1/src/main/java/start/HelloThread.java
@@ -0,0 +1,9 @@
+package start;
+
+public class HelloThread extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " : run()");
+    }
+}
