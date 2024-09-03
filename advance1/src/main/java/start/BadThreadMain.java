c474255ddccd42a4175ca98f3c9aee34999d1372
developiaa <developiaa@gmail.com>
2024-09-03 20:00:00 +0900

    feat: [advance1] 데몬 스레드

---
+++ advance1/src/main/java/start/BadThreadMain.java
@@ -0,0 +1,14 @@
+package start;
+
+public class BadThreadMain {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+        helloThread.run(); // main thread가 직접 실행함
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
