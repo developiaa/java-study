7a4391947f6355f02c77f73c17f59cac159cd0e6
developiaa <developiaa@gmail.com>
2024-09-07 08:00:00 +0900

    feat: [advance1] Runnable을 만드는 다양한 방법

---
+++ advance1/src/main/java/start/InnerRunnableMainV3.java
@@ -0,0 +1,12 @@
+package start;
+
+import static util.MyLogger.log;
+
+public class InnerRunnableMainV3 {
+    public static void main(String[] args) {
+        log("main() start");
+        new Thread(() -> log("run()")).start();
+        log("main() end");
+    }
+
+}
