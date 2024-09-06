61515a07d24f5f4017f4a3310ea78d6030a6da53
developiaa <developiaa@gmail.com>
2024-09-06 20:00:00 +0900

    feat: [advance1] 여러 스레드 만들기

--- advance1/src/main/java/start/HelloRunnable.java
+++ advance1/src/main/java/start/HelloRunnable.java
@@ -1,8 +1,11 @@
 package start;
 
+import static util.MyLogger.log;
+
 public class HelloRunnable implements Runnable{
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + ": run()");
+//        System.out.println(Thread.currentThread().getName() + ": run()");
+        log("runnable() run()");
     }
 }
