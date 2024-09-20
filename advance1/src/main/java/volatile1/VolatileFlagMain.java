340f3dead02c05a5ed9074914c09f1d582f0ad2d
developiaa <developiaa@gmail.com>
2024-09-20 20:30:00 +0900

    feat: [advance1] volatile 메모리 가시성3

--- advance1/src/main/java/volatile1/VolatileFlagMain.java
+++ advance1/src/main/java/volatile1/VolatileFlagMain.java
@@ -18,14 +18,26 @@ public class VolatileFlagMain {
     }
 
     static class MyTask implements Runnable {
-        boolean runFlag = true;
-        // volatile boolean runFlag = true;
+        // boolean runFlag = true;
+
+        /**
+         * 캐시 메모리를 무시하고 메인 메모리에 직접 접근
+         * 여러 스레드에서 같은 값을 읽고 써야하는 경우 사용
+         * 캐시 메모리보단 성능이 느리다는 단점이 있으므로 반드시 사용할 곳에 사용해야 함
+         */
+        volatile boolean runFlag = true;
 
         @Override
         public void run() {
             log("task 시작");
             while (runFlag) {
                 // runFlag가 false로 변하면 탈출
+
+                /**
+                 * 안에 어떤 코드를 넣게 되면 컨텍스트 스위칭이 발생되어
+                 * 캐시 메모리를 지우기 때문에 volatile 없이도 종료될 수 있다.
+                 * 단, 컨텍스트 스위칭이 발생되더라도 캐시 메모리를 지우는 것을 보장하지 않는다.
+                 */
             }
             log("task 종료");
         }
