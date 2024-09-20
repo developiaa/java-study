9bf40ab746b17834676f4423a1e4deaac1874655
developiaa <developiaa@gmail.com>
2024-09-21 08:30:00 +0900

    feat: [advance1] synchronized 메서드

--- advance1/src/main/java/sync/BankMain.java
+++ advance1/src/main/java/sync/BankMain.java
@@ -5,9 +5,10 @@ import static util.ThreadUtils.sleep;
 
 public class BankMain {
     public static void main(String[] args) throws InterruptedException {
-        BankAccountV1 bankAccountV1 = new BankAccountV1(1000);
-        Thread t1 = new Thread(new WithdrawTask(bankAccountV1, 800), "t1");
-        Thread t2 = new Thread(new WithdrawTask(bankAccountV1, 800), "t2");
+//        BankAccountV1 account = new BankAccountV1(1000);
+        BankAccountV2 account = new BankAccountV2(1000);
+        Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
         t1.start();
         t2.start();
         sleep(500);// 검증 완료까지 잠시 대기
@@ -16,6 +17,6 @@ public class BankMain {
         t1.join();
         t2.join();
 
-        log("최종 잔액 : " + bankAccountV1.getBalance());
+        log("최종 잔액 : " + account.getBalance());
     }
 }
