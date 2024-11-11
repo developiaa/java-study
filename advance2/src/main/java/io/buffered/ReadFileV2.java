05f1cba10a12492ab1afc64e62ae1636e51e2efc
developiaa <developiaa@gmail.com>
2024-11-11 20:00:00 +0900

    feat: [advance2] 파일 입출력과 성능 최적화2 - 버퍼 활용

--- advance2/src/main/java/io/buffered/ReadFileV2.java
+++ advance2/src/main/java/io/buffered/ReadFileV2.java
@@ -3,18 +3,18 @@ package io.buffered;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import static io.buffered.BufferedConst.FILE_NAME;
-import static io.buffered.BufferedConst.FILE_SIZE;
+import static io.buffered.BufferedConst.*;
 
-public class ReadFileV1 {
+public class ReadFileV2 {
     public static void main(String[] args) throws IOException {
         FileInputStream fis = new FileInputStream(FILE_NAME);
         long startTime = System.currentTimeMillis();
 
+        byte[] buffer = new byte[BUFFER_SIZE];
         int fileSize = 0;
-        int data;
-        while ((data = fis.read()) != -1) {
-            fileSize++;
+        int size;
+        while ((size = fis.read(buffer)) != -1) {
+            fileSize += size;
         }
         fis.close();
 
